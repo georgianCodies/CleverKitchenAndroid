070142de3bfc9d576ccd5e7ce70bfb4aede9be82
Palika-Sathish <113234133+Palika-Sathish@users.noreply.github.com>
2022-11-12 11:04:46 -0500

    upload video button updated

--- app/src/main/java/com/mdev/cleverkitchenandroid/AddRecipeFragment.kt
+++ app/src/main/java/com/mdev/cleverkitchenandroid/AddRecipeFragment.kt
@@ -1,21 +1,32 @@
 package com.mdev.cleverkitchenandroid
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.ValueCallback
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-
+import java.io.File
 
 class AddRecipeFragment : Fragment() {
 
+    private val sharedPrefFile = "kotlinsharedpreference"
     private lateinit var recipeName: TextView
     private lateinit var ingredients: TextView
     private lateinit var description: TextView
     private lateinit var video: TextView
+
+    var part_image: String? = null
+
+
+    var filePath: ValueCallback<Array<Uri>>? = null
 
 
     override fun onCreateView(
@@ -31,9 +42,13 @@ class AddRecipeFragment : Fragment() {
         recipeName = view.findViewById<TextView>(R.id.recipeNameEditText);
         ingredients = view.findViewById<TextView>(R.id.ingredientsEditText)
         description = view.findViewById<TextView>(R.id.descriptionEditText)
-        video = view.findViewById<TextView>(R.id.videoInputText);
+        video = view.findViewById<Button>(R.id.videoInputButton);
 
         var isAllFieldsChecked = false
+
+        video.setOnClickListener {
+            println("clicked on video upload!");
+        }
 
 
         submitButton.setOnClickListener(View.OnClickListener { // store the returned value of the dedicated function which checks
@@ -49,12 +64,31 @@ class AddRecipeFragment : Fragment() {
                 println(recipeName.text)
                 println(ingredients.text)
                 println(description.text)
+
+                val sharedPreferences: SharedPreferences =
+                    context?.getSharedPreferences(sharedPrefFile,Context.MODE_PRIVATE)!!
+                    //val id:Int = Integer.parseInt(inputId.text.toString())
+                   // val name:String = inputName.text.toString()
+                val editor:SharedPreferences.Editor =  sharedPreferences.edit()
+
+                editor.putString("recipeName",recipeName.text.toString())
+                editor.putString("ingredients",ingredients.text.toString())
+                editor.putString("description",description.text.toString())
+
+                editor.apply()
+                editor.commit()
+
+                println("Saved data is:")
+                val savedRecipeName = sharedPreferences.getString("recipeName","no values")
+                println(savedRecipeName)
             }
         })
 
 
         return view
     }
+
+
 
     private fun checkAllFields(): Boolean {
         if (recipeName.length() === 0) {
