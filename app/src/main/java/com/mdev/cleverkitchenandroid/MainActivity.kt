16bc56de3fdd796f6b8d55d0efd2d2d3b72e88ae
Chandra Prakash Bommadevara <chandraprakashb15@gmail.com>
2022-11-22 02:16:11 -0500

    feat: bottom navigation created and strings added

--- app/src/main/java/com/mdev/cleverkitchenandroid/MainActivity.kt
+++ app/src/main/java/com/mdev/cleverkitchenandroid/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.mdev.cleverkitchenandroid
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation_menu)
+        navView.setupWithNavController(navController)
 
     }
 
