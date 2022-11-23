d198b6399b645ad760f655d342e2be01851056df
Chandra Prakash Bommadevara <chandraprakashb15@gmail.com>
2022-11-23 10:26:41 -0500

    feat: created database helper for recipe details

--- app/src/main/java/com/mdev/cleverkitchenandroid/MainActivity.kt
+++ app/src/main/java/com/mdev/cleverkitchenandroid/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.mdev.cleverkitchenandroid
 
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
-
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         val navView: BottomNavigationView = findViewById(R.id.bottom_navigation_menu)
         navView.setupWithNavController(navController)
+
 
     }
 
