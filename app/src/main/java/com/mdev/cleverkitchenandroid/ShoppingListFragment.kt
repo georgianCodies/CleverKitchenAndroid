ce3c983576f0bb0a2e086869c3ebd75a009ed27c
Chandra Prakash Bommadevara <chandraprakashb15@gmail.com>
2022-11-26 22:00:20 -0500

    feat: created profile and shopping list fragment

---
+++ app/src/main/java/com/mdev/cleverkitchenandroid/ShoppingListFragment.kt
@@ -0,0 +1,18 @@
+package com.mdev.cleverkitchenandroid
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ShoppingListFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
