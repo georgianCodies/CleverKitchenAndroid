77e2748e39477496d483235f21f949e29e0bcbb8
harish3737 <37335142+harish3737@users.noreply.github.com>
2022-12-02 00:57:26 -0500

    Feat:CallforValidationApiandCallBack

--- app/src/main/java/com/mdev/cleverkitchenandroid/fragments/auth/SignUpFragment.kt
+++ app/src/main/java/com/mdev/cleverkitchenandroid/fragments/auth/SignUpFragment.kt
@@ -1,24 +1,32 @@
 package com.mdev.cleverkitchenandroid.fragments.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
 import com.mdev.cleverkitchenandroid.R
 import com.mdev.cleverkitchenandroid.database.CleverKitchenDatabase
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
 
+
+interface OnJSONResponseCallback {
+    fun onJSONResponse(success: Boolean, response: Boolean)
+}
 
 class SignUpFragment : Fragment() {
     var name:String = ""
     var email:String = ""
     var password:String = ""
     var confirmPassword:String = ""
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,18 +44,17 @@ class SignUpFragment : Fragment() {
             email = emailTextView.text.toString()
             password = passwordTextView.text.toString()
             confirmPassword = confirmPasswordTextView.text.toString()
-            if(validateFields()){
-                if(database.checkEmail(email)) {
-                    database.insertUser(email, name, password)
-                    view.findNavController().popBackStack()
+            verifyEmailPattern(email) { isEmailValid ->
+                if(isEmailValid as Boolean && validateFields()){
+                    if(database.checkEmail(email)) {
+                        database.insertUser(email, name, password)
+                        view.findNavController().popBackStack()
+                    }
+                    else{
+                        Toast.makeText(this@SignUpFragment.requireActivity(), "Email Id already exists", Toast.LENGTH_SHORT).show()
+                    }
                 }
-                else{
-                    Toast.makeText(this@SignUpFragment.requireActivity(), "Email Id already exists", Toast.LENGTH_SHORT).show()
-                }
-            }else{
-                Toast.makeText(this@SignUpFragment.requireActivity(), "Email Id already exists", Toast.LENGTH_SHORT).show()
             }
-
         }
         val signInTextview =  view.findViewById<TextView>(R.id.signInInSignUpTextView)
         signInTextview.setOnClickListener{
@@ -58,23 +65,57 @@ class SignUpFragment : Fragment() {
 
     private fun validateFields(): Boolean {
 
-        if (name == "") {
-            Toast.makeText(this@SignUpFragment.requireActivity(), "Please enter the name", Toast.LENGTH_SHORT).show()
+        if(name.isEmpty()){
+            Toast.makeText(this@SignUpFragment.requireActivity(), "Please enter your name", Toast.LENGTH_SHORT).show()
             return false
-        } else if (email == "") {
-            Toast.makeText(this@SignUpFragment.requireActivity(), "Please enter your email id", Toast.LENGTH_SHORT).show()
+        } else if(password.isEmpty()){
+            Toast.makeText(this@SignUpFragment.requireActivity(), "Please enter your password", Toast.LENGTH_SHORT).show()
             return false
-        } else if (password == "") {
-            Toast.makeText(this@SignUpFragment.requireActivity(), "Please enter the password", Toast.LENGTH_SHORT).show()
-            return false
-        } else if (confirmPassword == "") {
-            Toast.makeText(this@SignUpFragment.requireActivity(), "Please confirm the password", Toast.LENGTH_SHORT).show()
-            return false
-        } else if (password != confirmPassword) {
+        }
+        else if (password.isNotEmpty() && confirmPassword.isNotEmpty() && password != confirmPassword) {
             Toast.makeText(this@SignUpFragment.requireActivity(), "Passwords doesn't match", Toast.LENGTH_SHORT).show()
             return false
         }
 
         return true
+    }
+
+    private  fun verifyEmailPattern(email:String, callback: (Boolean?) -> Unit) {
+        val url = "https://emailvalidation.abstractapi.com/v1/?" +
+                "api_key=c085091fd2434a3ea2bf6c0c19e97c14&" +
+                "email="+email;
+        var isEmailValid = false
+
+        AsyncHttpClient().get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<Header?>?, responseBody: ByteArray?) {
+                val response = String(responseBody!!)
+
+                val obj = JSONObject(response)
+                isEmailValid = obj.getJSONObject("is_valid_format").getBoolean("value")
+                callback.invoke(isEmailValid);
+                if(!isEmailValid) {
+                    Toast.makeText(
+                        this@SignUpFragment.requireActivity(),
+                        "Please enter valid email id",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<Header?>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                Log.d("response",error.toString())
+                callback.invoke(isEmailValid);
+                Toast.makeText(
+                    this@SignUpFragment.requireActivity(),
+                    "Please enter valid email id",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 }
