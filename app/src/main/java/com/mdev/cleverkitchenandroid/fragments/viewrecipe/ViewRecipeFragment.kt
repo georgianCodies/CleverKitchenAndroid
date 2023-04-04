c517077376e131e921a1eb2a44c82f8a1a0276fe
Manmohan Singh <er.manmohansingh7@gmail.com>
2023-04-03 20:43:14 -0400

    favorites switcher in view recipes

--- app/src/main/java/com/mdev/cleverkitchenandroid/fragments/viewrecipe/ViewRecipeFragment.kt
+++ app/src/main/java/com/mdev/cleverkitchenandroid/fragments/viewrecipe/ViewRecipeFragment.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filter.FilterListener
+import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mdev.cleverkitchenandroid.R
@@ -24,6 +30,7 @@ class ViewRecipeFragment : Fragment() {
         val sharedPreferences = activity?.getSharedPreferences("userDetails", Context.MODE_PRIVATE)
         val emailId = sharedPreferences?.getString("emailId","")
         println("emailId:  " + emailId)
+
         val dataBaseArrayList:ArrayList<Recipe> =  databaseClass.getRecipeDetails(emailId.toString())
         println(dataBaseArrayList.toString())
         var userDetails:User =  databaseClass.getUser(emailId.toString())
@@ -31,8 +38,27 @@ class ViewRecipeFragment : Fragment() {
 
         val rvRecipies: RecyclerView = view.findViewById(R.id.rv_recipies)
         rvRecipies.layoutManager = LinearLayoutManager(view.context)
-        val receipiesAdapter = FragmentViewRecipeAdapter(dataBaseArrayList,userDetails,databaseClass)
-        rvRecipies.adapter = receipiesAdapter
+
+        val receipesAdapter = FragmentViewRecipeAdapter(dataBaseArrayList, userDetails, databaseClass)
+        rvRecipies.adapter = receipesAdapter
+
+        val noFavRecipe = view.findViewById<TextView>(R.id.no_fav_recipes)
+
+        val switch = view.findViewById<SwitchCompat>(R.id.material_switch)
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                receipesAdapter.filter.filter("showFav") { i ->
+                    if (i == 0) {
+                        noFavRecipe.visibility = VISIBLE
+                    } else {
+                        noFavRecipe.visibility = INVISIBLE
+                    }
+                }
+            } else {
+                receipesAdapter.filter.filter("")
+                noFavRecipe.visibility = INVISIBLE
+            }
+        }
 
         return view
     }
