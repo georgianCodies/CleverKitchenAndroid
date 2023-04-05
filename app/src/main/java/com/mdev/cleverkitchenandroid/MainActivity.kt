c989cd9258c53529dda4aef857bae77c45193b8a
Manmohan Singh <er.manmohansingh7@gmail.com>
2023-04-05 19:54:02 -0400

    top bar navigation

--- app/src/main/java/com/mdev/cleverkitchenandroid/MainActivity.kt
+++ app/src/main/java/com/mdev/cleverkitchenandroid/MainActivity.kt
@@ -6,24 +6,37 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val navView: BottomNavigationView = findViewById(R.id.bottom_navigation_menu)
         navView.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            navView.visibility = if(destination.id != R.id.homeFragment && destination.id != R.id.profileFragment) {
-                View.GONE
+            val ids = arrayOf(R.id.viewRecipeFragment, R.id.recipeDetailsFragment, R.id.addRecipeFragment, R.id.shoppingListFragment)
+
+            if(Arrays.stream(ids).anyMatch { t -> t == destination.id }) {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                supportActionBar?.setDisplayShowHomeEnabled(true)
             } else {
-                View.VISIBLE
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                supportActionBar?.setDisplayShowHomeEnabled(false)
             }
+
+            navView.visibility = if(destination.id != R.id.homeFragment && destination.id != R.id.profileFragment) View.GONE else View.VISIBLE
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
